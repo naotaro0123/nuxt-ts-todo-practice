239fa85493d9606acc9066ce29710812af2d67d6
naotaro0123 <n.ikewada@gmail.com>
2019-04-29 16:48:21 +0900

    チュートリアル vol.1

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,44 @@
 <template>
   <section class="container">
+    <Logo />
+    <h1>Todo App</h1>
+    <p><input type="text" name="content" /></p>
     <div>
-      <logo />
-      <h1 class="title">
-        todo
-      </h1>
-      <h2 class="title-subtitle">
-        My great Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
+      <button>save</button>
+      <button>find</button>
     </div>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <span>{{ todo.content }}</span>
+        <span>({{ todo.created }})</span>
+        <span>x</span>
+      </li>
+    </ul>
   </section>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
+import Vue from 'vue';
+import { mapState } from 'vuex';
+import Logo from '~/components/Logo.vue';
 
 export default Vue.extend({
   components: {
     Logo
+  },
+  data() {
+    return {
+      content: ''
+    };
+  },
+  computed: {
+    ...mapState(['todos'])
   }
-})
+});
 </script>
 
 <style lang="scss">
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-
-  &-subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-}
-
-.links {
-  padding-top: 15px;
+  margin: 50px;
 }
 </style>
---
+++ store/index.ts
@@ -0,0 +1,57 @@
+/* eslint-disable no-console */
+import Vuex from 'vuex';
+
+interface Todo {
+  content: string;
+  created: string;
+}
+
+const todoList: Todo[] = [
+  {
+    content: 'hogehoge',
+    created: '2019-03-31 15:30'
+  },
+  {
+    content: 'fugafuga',
+    created: '2019-03-31 16:00'
+  }
+];
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: () => ({
+      todos: todoList
+    }),
+    mutations: {
+      insert: (state, todo: Todo) => {
+        const date = new Date();
+        const formatedDate = `
+          ${date.getFullYear()}
+          -${('00' + (date.getMonth() + 1)).slice(-2)}
+          -${('00' + (date.getDate() + 1)).slice(-2)}
+          -${('00' + (date.getHours() + 1)).slice(-2)}
+          :${('00' + (date.getMinutes() + 1)).slice(-2)}
+        `;
+        state.todos.unshift({
+          content: todo.content,
+          created: formatedDate
+        });
+      },
+      remove: (state, todo: Todo) => {
+        for (let i = 0; i < state.todos.length; i++) {
+          const stateTodos = state.todos[i];
+          if (
+            stateTodos.content === todo.content &&
+            stateTodos.created === todo.created
+          ) {
+            console.log(`remove ${todo.content}`);
+            state.todos.splice(i, 1);
+            return;
+          }
+        }
+      }
+    }
+  });
+};
+
+export default createStore;
